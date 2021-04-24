a14d9d52e32db4708f5b47f76959c339e6f9261a
AykutSarac <aykutsarac0@gmail.com>
2021-04-24 16:26:03 +0300

    completed sales list

--- src/components/Store/StoreSales.js
+++ src/components/Store/StoreSales.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import newsData from '../../assets/News/newsData.json'
+import './StoreSales.css'
 
 const StoreSales = () => {
     return (
-        <div>
+        <section className="sales">
             <h3>Games On Sale</h3>
-        </div>
+            <ul className="saleslist">
+                {newsData.slice(6, 10).map((game) => (
+                    <li key={game._id} className="card">
+                        <a href={'https://www.epicgames.com/store/en-US' + game.url}>
+                            <img src={game._images_[0]} alt="sales game" />
+                            <div className="card-text">
+                                <p>{game._title.split(' ').slice(0, 4).join(' ')}<br /><span className="author">{game.author ? game.author : 'Game Author'}</span></p>
+                                <span className="pricetag">-25%</span><span className="oldprice">50 $</span> 37 $
+                            </div>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </section>
     )
 }
 
