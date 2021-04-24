12bdaca3f1b6b3f82004663ddf4ccea93d4cd85d
AykutSarac <aykutsarac0@gmail.com>
2021-04-24 18:21:10 +0300

    Routing adjustments

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Sidebar />
         <Sidecontent>
